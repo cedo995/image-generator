ba0ddeb6abddbde4a1fe55bc3993ee5547690674
cedo995 <c.lazovic@gmail.com>
2023-01-27 14:08:21 +0100

    first commit

---
+++ frontend/src/utils/index.js
@@ -0,0 +1,10 @@
+import { surpriseMePrompts } from '../constants'
+
+export function getRandomPrompt(prompt) {
+    const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+    const RandomPrompt = surpriseMePrompts[randomIndex];
+
+    if (RandomPrompt === prompt) return getRandomPrompt(prompt)
+
+    return RandomPrompt
+}
