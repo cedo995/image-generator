ffc9bbccad73eebb99d8a7a27da0a44c493ae131
cedo_home <c.lazovic@gmail.com>
2023-01-30 17:50:32 +0100

    commit_home_4

--- server/index.js
+++ server/index.js
@@ -1,2 +1,19 @@
 import { Express } from "express";
-import * 
+import * as dotenv from 'dotenv';
+import cors from 'cors'
+
+dotenv.config();
+
+const app = Express();
+app.use(cors());
+app.use(express.json({limit:'50mb'}));
+
+app.get('/', (req,res) => {
+    res.send('Hello from DALL-E!')
+});
+
+const startServer = async () => {
+    app.listen(8080, ()=> console.log('Server listening on port 8080));
+};
+
+startServer();
